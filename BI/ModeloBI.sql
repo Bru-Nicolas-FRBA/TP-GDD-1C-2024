5301a669a21580443d1ab051391178566418cdfd
Bru Nicolas <82009198+Bru-Nicolas-FRBA@users.noreply.github.com>
2024-07-09 12:14:55 -0300

    Update ModeloBI.sql

--- BI/ModeloBI.sql
+++ BI/ModeloBI.sql
@@ -54,13 +54,72 @@ IF OBJECT_ID('BI_REYES_DE_DATOS.turno', 'FN') IS NOT NULL BEGIN DROP FUNCTION BI
 IF OBJECT_ID('BI_REYES_DE_DATOS.rangoEtario ', 'FN') IS NOT NULL BEGIN DROP FUNCTION BI_REYES_DE_DATOS.rangoEtario ; END
 IF OBJECT_ID('BI_REYES_DE_DATOS.cuatrimestre', 'FN') IS NOT NULL BEGIN DROP FUNCTION BI_REYES_DE_DATOS.cuatrimestre; END
 GO
------ ----- ----- ----- ----- ----- ----- ----- 
------ CREACIÓN DE TABLAS-DIMENSIONES ----- 
------ ----- ----- ----- ----- ----- ----- ----- 
-------------------------------------------------------------
+------------------------------------------------------------ esquema
 IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'BI_REYES_DE_DATOS')
 BEGIN EXEC ('CREATE SCHEMA BI_REYES_DE_DATOS') END
 GO
+----- ----- ----- ----- ----- ----
+-----  CREACIÓN DE FUNCIONES -----
+----- ----- ----- ----- ----- ----- 
+------------------------------------------------------------ Turno
+CREATE FUNCTION BI_REYES_DE_DATOS.turno(@hora TIME)
+RETURNS VARCHAR(50)
+AS
+BEGIN
+    DECLARE @turno VARCHAR(50);
+    SELECT @turno = 
+        CASE 
+            WHEN @hora BETWEEN '08:00:00' AND '11:59:59' THEN 'Turno de 8 a 12'
+            WHEN @hora BETWEEN '12:00:00' AND '15:59:59' THEN 'Turno de 12 a 16'
+            WHEN @hora BETWEEN '16:00:00' AND '20:00:00' THEN 'Turno de 16 a 20'
+            ELSE 'Fuera de turno'
+        END;
+    RETURN @turno;
+END;
+GO
+
+------------------------------------------------------------ Rango Etario
+CREATE FUNCTION BI_REYES_DE_DATOS.rangoEtario(@fechaNacimiento AS DATE)
+RETURNS VARCHAR(30)
+AS
+BEGIN
+	DECLARE @edad INT
+	SET @edad = YEAR(GETDATE()) - YEAR(@fechaNacimiento) 
+	DECLARE @rangoEtario INT
+	
+	IF(@edad < 25)
+		SET @rangoEtario = 'Menor a 25'
+	IF(@edad BETWEEN 25 AND 34)
+		SET @rangoEtario = '25 a 35'
+	IF(@edad BETWEEN 35 AND 50)
+		SET @rangoEtario = '35 a 50'
+	IF(@edad > 50)
+		SET @rangoEtario = 'Mas de 50'
+
+RETURN @rangoEtario
+END
+GO
+------------------------------------------------------------ Cuatrimestre
+CREATE FUNCTION BI_REYES_DE_DATOS.cuatrimestre(@mes INT)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @cuatrimestre INT
+
+	IF(@mes BETWEEN 1 AND 4)
+	SET @cuatrimestre = 1
+	IF (@mes BETWEEN 5 AND 8)
+	SET @cuatrimestre = 2
+	IF (@mes BETWEEN 9 AND 12)
+	SET @cuatrimestre = 3
+
+ RETURN @cuatrimestre
+	
+END
+GO
+----- ----- ----- ----- ----- ----- ----- ----- 
+----- CREACIÓN DE TABLAS-DIMENSIONES ----- 
+----- ----- ----- ----- ----- ----- ----- -----
 ------
 CREATE TABLE BI_REYES_DE_DATOS.BI_Tiempo(
 	id_tiempo INT PRIMARY KEY IDENTITY(1,1),
@@ -117,8 +176,8 @@ CREATE TABLE BI_REYES_DE_DATOS.BI_hechos_envio (
   	id_tiempo int not null,
 	id_ubicacion int not null,
 	id_sucursal int not null,
-	id_rango_etario int not null,
-	id_turno int not null,
+	id_turno varchar(50) not null,
+	id_rango_etario varchar(30) not null,
     envio_fecha_programada DATETIME NULL,
     envio_fecha_entrega DATETIME NOT NULL,
     envio_costo DECIMAL(10, 2) NOT NULL,
@@ -129,20 +188,25 @@ CREATE TABLE BI_REYES_DE_DATOS.BI_hechos_venta(
   	id_tiempo INT NOT NULL,
 	id_ubicacion INT NOT NULL,
 	id_sucursal INT NOT NULL,
-	id_turno INT NOT NULL,
+	id_turno varchar(50) NOT NULL,
+	id_rango_etario varchar(30) NOT NULL,
+	id_categoria_prod INT NOT NULL,
+	id_subcategoria INT NOT NULL,
 	--id_medio_de_pago INT NOT NULL,
     venta_id_caja INT NOT NULL,
     monto_ventas DECIMAL(10, 2) NOT NULL,
+	cantidad_ventas int not null,
     monto_descuentos_aplicados DECIMAL(10, 2),
+	cantidad_descuentos int not null
 );
 -----
 CREATE TABLE BI_REYES_DE_DATOS.BI_hechos_pago (
   	id_pago INT PRIMARY KEY IDENTITY(1,1),
   	id_tiempo INT NOT NULL,
 	--id_ubicacion INT NOT NULL,
-	--id_sucursal INT NOT NULL,
-	--id_rango_etario INT NOT NULL,
-	id_turno INT NOT NULL,
+	id_sucursal INT NOT NULL,
+	id_turno varchar(50) NOT NULL,
+	id_rango_etario varchar(30) NOT NULL,
 	id_medio_de_pago INT NOT NULL,
   	monto_pagos INT NOT NULL,
 	cantidad_pagos INT NOT NULL,
@@ -168,65 +232,6 @@ ALTER TABLE BI_REYES_DE_DATOS.BI_Producto ADD CONSTRAINT FK_id_producto_categori
 ALTER TABLE BI_REYES_DE_DATOS.BI_Producto ADD CONSTRAINT FK_id_producto_subcategoria FOREIGN KEY (id_producto_subcategoria) REFERENCES BI_REYES_DE_DATOS.BI_Producto_subcategoria(id_producto_subcategoria)
 ALTER TABLE BI_REYES_DE_DATOS.BI_Empleado ADD CONSTRAINT FK_id_sucursal_empleado FOREIGN KEY (id_sucursal) REFERENCES BI_REYES_DE_DATOS.BI_Sucursal(id_sucursal)
 ALTER TABLE BI_REYES_DE_DATOS.BI_Cliente ADD CONSTRAINT FK_cliente_id_domicilio FOREIGN KEY (cliente_id_domicilio) REFERENCES BI_REYES_DE_DATOS.BI_Ubicacion(id_ubicacion)
-GO
------ ----- ----- ----- ----- ----
------  CREACIÓN DE FUNCIONES -----
------ ----- ----- ----- ----- ----- 
------------------------------------------------------------- Turno
-CREATE FUNCTION BI_REYES_DE_DATOS.turno(@hora TIME)
-RETURNS VARCHAR(50)
-AS
-BEGIN
-    DECLARE @turno VARCHAR(50);
-    SELECT @turno = 
-        CASE 
-            WHEN @hora BETWEEN '08:00:00' AND '11:59:59' THEN 'Turno de 8 a 12'
-            WHEN @hora BETWEEN '12:00:00' AND '15:59:59' THEN 'Turno de 12 a 16'
-            WHEN @hora BETWEEN '16:00:00' AND '20:00:00' THEN 'Turno de 16 a 20'
-            ELSE 'Fuera de turno'
-        END;
-    RETURN @turno;
-END;
-GO
-
------------------------------------------------------------- Rango Etario
-CREATE FUNCTION BI_REYES_DE_DATOS.rangoEtario(@fechaNacimiento AS DATE)
-RETURNS INT
-AS
-BEGIN
-	DECLARE @edad INT
-	SET @edad = YEAR(GETDATE()) - YEAR(@fechaNacimiento) 
-	DECLARE @rangoEtario INT
-	
-	IF(@edad < 25)
-		SET @rangoEtario = 1
-	IF(@edad BETWEEN 25 AND 34)
-		SET @rangoEtario = 2
-	IF(@edad BETWEEN 35 AND 50)
-		SET @rangoEtario = 3
-	IF(@edad > 50)
-		SET @rangoEtario = 4
-
-RETURN @rangoEtario
-END
-GO
------------------------------------------------------------- Cuatrimestre
-CREATE FUNCTION BI_REYES_DE_DATOS.cuatrimestre(@mes INT)
-RETURNS INT
-AS
-BEGIN
-	DECLARE @cuatrimestre INT
-
-	IF(@mes BETWEEN 1 AND 4)
-	SET @cuatrimestre = 1
-	IF (@mes BETWEEN 5 AND 8)
-	SET @cuatrimestre = 2
-	IF (@mes BETWEEN 9 AND 12)
-	SET @cuatrimestre = 3
-
- RETURN @cuatrimestre
-	
-END
 GO
 ----- ----- ----- ----- ----- ----- ----- ----- 
 -----  CREACIÓN DE MIGRACIONES-DIMENSIONES -----
@@ -355,19 +360,28 @@ INSERT INTO BI_REYES_DE_DATOS.BI_hechos_venta(
 	id_ubicacion,
 	id_sucursal,
 	id_turno,
-	--id_medio_de_pago,
+	id_rango_etario,
+	id_categoria_prod,
+	id_subcategoria,
     venta_id_caja,
     monto_ventas,
-    monto_descuentos_aplicados
+	cantidad_ventas,
+    monto_descuentos_aplicados,
+	cantidad_descuentos
 )
 SELECT
 	tp.id_tiempo,
 	u.id_ubicacion,
 	s.id_sucursal,
 	BI_REYES_DE_DATOS.turno(CAST(t.ticket_fecha_hora AS TIME)),
+	BI_REYES_DE_DATOS.rangoEtario(e.empleado_fecha_nacimiento),
+	p.id_producto_categoria,
+	p.id_producto_subcategoria,
 	t.id_caja,
 	sum(t.ticket_total),
-	sum(t.ticket_total_descuento_aplicado)
+	count(t.ticket_total),
+	sum(t.ticket_total_descuento_aplicado),
+	count(t.ticket_total_descuento_aplicado)
 FROM REYES_DE_DATOS.Ticket t
 	JOIN BI_REYES_DE_DATOS.BI_Tiempo tp on
 		year(t.ticket_fecha_hora) = tp.anio
@@ -375,6 +389,9 @@ FROM REYES_DE_DATOS.Ticket t
 		and BI_REYES_DE_DATOS.cuatrimestre(month(t.ticket_fecha_hora)) = tp.cuatrimestre
 	JOIN BI_REYES_DE_DATOS.BI_Sucursal s on s.id_sucursal = t.id_sucursal
 	JOIN BI_REYES_DE_DATOS.BI_Ubicacion u on u.direccion = s.sucursal_domicilio
+	JOIN REYES_DE_DATOS.Empleado e on t.id_empleado = e.id_empleado
+	JOIN REYES_DE_DATOS.Item_Ticket i on i.ticket_numero+i.id_sucursal+i.id_tipo_comprobante = t.ticket_numero+t.id_sucursal+t.id_tipo_comprobante
+	JOIN REYES_DE_DATOS.Producto p on i.id_producto = p.id_producto
 GROUP BY tp.id_tiempo,
 	u.id_ubicacion,
 	s.id_sucursal,
@@ -386,7 +403,7 @@ GO
 INSERT INTO BI_REYES_DE_DATOS.BI_hechos_pago(
 	id_tiempo,
 	--id_ubicacion,
-	--id_sucursal,
+	id_sucursal,
 	--id_rango_etario,
 	id_turno,
 	id_medio_de_pago,
@@ -395,6 +412,7 @@ INSERT INTO BI_REYES_DE_DATOS.BI_hechos_pago(
 )
 SELECT
 	t.id_tiempo,
+	select * from REYES_DE_DATOS.Pago
 	BI_REYES_DE_DATOS.turno(CAST(p.pago_fecha AS TIME)),
 	p.id_tipo_medio_de_pago,
 	sum(p.pago_importe),
@@ -440,15 +458,13 @@ GO
 -----
 CREATE VIEW BI_REYES_DE_DATOS.BI_Vista_Ticket_Promedio_Mensual AS
 SELECT
-    AVG(v.venta_total) AS PromedioMensual,
+    AVG(v.monto_ventas) AS PromedioMensual,
     l.localidad_nombre,
     t.anio AS Año,
     t.mes AS Mes
 FROM BI_REYES_DE_DATOS.BI_hechos_venta v
-	JOIN BI_REYES_DE_DATOS.BI_hechos_venta_tiempo vt ON v.id_venta = vt.id_venta
-	JOIN BI_REYES_DE_DATOS.BI_Tiempo t ON vt.id_tiempo = t.id_tiempo
-	JOIN BI_REYES_DE_DATOS.BI_hechos_venta_ubicacion vu ON v.id_venta = vu.id_venta
-	JOIN BI_REYES_DE_DATOS.BI_Ubicacion u ON vu.id_ubicacion = u.id_ubicacion
+	JOIN BI_REYES_DE_DATOS.BI_Tiempo t ON v.id_tiempo = t.id_tiempo
+	JOIN BI_REYES_DE_DATOS.BI_Ubicacion u ON v.id_ubicacion = u.id_ubicacion
 	join REYES_DE_DATOS.Localidad l on u.id_localidad = l.id_localidad
 GROUP BY
     t.mes,
@@ -461,76 +477,63 @@ GO
 ----- 
 CREATE VIEW BI_REYES_DE_DATOS.Vista_Cantidad_Unidades_Promedio AS
 SELECT
-	--promedio cantidad unidades
-	avg(t.item_ticket_cantidad) as Promedio,
-	--para cada turno
-	BI_REYES_DE_DATOS.turno(CAST(v.ticket_fecha_hora AS TIME)) as Turno,
-    --para cada cuatrimestre
-	tmp.cuatrimestre as Cuatrimestre,
-    --para cada anio
-	tmp.anio as Año
+	avg(v.cantidad_ventas) as Promedio,
+	v.id_turno as Turno,
+    t.cuatrimestre as Cuatrimestre,
+    t.anio as Año
 FROM BI_REYES_DE_DATOS.BI_hechos_venta v
-	join BI_REYES_DE_DATOS.BI_hechos_venta_tiempo vt on v.id_venta = vt.id_tiempo
-	join BI_REYES_DE_DATOS.BI_Tiempo tmp on vt.id_tiempo = tmp.id_tiempo
-	join BI_REYES_DE_DATOS.BI_hechos_ticket t on t.id_ticket = v.id_ticket
+	JOIN BI_REYES_DE_DATOS.BI_Tiempo t ON v.id_tiempo = t.id_tiempo
 GROUP BY
-	--3,
-	v.ticket_fecha_hora,
-	tmp.cuatrimestre,
-	tmp.anio;
+	v.id_turno,
+    t.cuatrimestre,
+    t.anio;
 GO
 ----- 
 -- 3) REVISAR Vista para calcular porcentaje anual de ventas registradas por rango etario del empleado según el tipo de caja para cada cuatrimestre.
 ----- 
 CREATE VIEW BI_REYES_DE_DATOS.BI_Porcentaje_Ventas_Por_Cuatrimestre AS
 SELECT
-    BI_REYES_DE_DATOS.rangoEtario(e.empleado_fecha_nacimiento) as RangoEtario,
+    v.id_rango_etario as RangoEtario,
     c.caja_tipo as TipoCaja,
     t.cuatrimestre as Cuatrimestre,
     (
-        SUM(v.venta_total) * 100
+        SUM(v.monto_ventas) * 100
         /
         (
-            SELECT SUM(v2.venta_total)
+            SELECT SUM(v2.monto_ventas)
                 FROM BI_REYES_DE_DATOS.BI_hechos_venta v2
-                    JOIN BI_REYES_DE_DATOS.BI_hechos_venta_tiempo vt2 ON v2.id_ticket = vt2.id_venta
-                    JOIN BI_REYES_DE_DATOS.BI_Tiempo t2 ON vt2.id_tiempo = t2.id_tiempo
-                WHERE
-                    t2.anio = t.anio
+                    JOIN BI_REYES_DE_DATOS.BI_Tiempo t2 ON v2.id_tiempo = t2.id_tiempo
+                WHERE t2.anio = t.anio
         )
     ) as Porcentaje
 FROM 
     BI_REYES_DE_DATOS.BI_hechos_venta v
-    JOIN BI_REYES_DE_DATOS.BI_hechos_venta_tiempo vt ON v.id_ticket = vt.id_venta
-    JOIN BI_REYES_DE_DATOS.BI_Tiempo t ON vt.id_tiempo = t.id_tiempo
-    JOIN BI_REYES_DE_DATOS.BI_Caja c ON c.id_caja = v.id_venta
-    JOIN BI_REYES_DE_DATOS.BI_Empleado e ON v.venta_id_empleado = e.id_empleado
+    JOIN BI_REYES_DE_DATOS.BI_Tiempo t ON v.id_tiempo = t.id_tiempo
+    JOIN REYES_DE_DATOS.Caja c ON c.id_caja = v.venta_id_caja
 GROUP BY
-    BI_REYES_DE_DATOS.rangoEtario(e.empleado_fecha_nacimiento),
+	v.id_rango_etario,
     c.caja_tipo,
     t.cuatrimestre,
-    t.anio;
+	t.anio;
 GO
 ----- 
 -- 4) Vista para calcular cantidad de ventas registradas por turno para cada localidad según el mes de cada año
 -----
 CREATE VIEW BI_REYES_DE_DATOS.Vista_Cantidad_Ventas_Por_Turno_Y_Localidad AS
 SELECT
+	v.id_turno as Turno,
     l.localidad_nombre as Localidad,
     t.mes as Mes,
     t.anio as Año,
-	BI_REYES_DE_DATOS.turno(CAST(v.ticket_fecha_hora AS TIME)) as Turno,
-    count(*) as CantidadVentas
+    sum(v.cantidad_ventas) as CantidadVentas
 FROM BI_REYES_DE_DATOS.BI_hechos_venta v
-    JOIN BI_REYES_DE_DATOS.BI_hechos_venta_tiempo vt ON vt.id_venta = v.id_venta
-	JOIN BI_REYES_DE_DATOS.BI_hechos_venta_ubicacion vu ON vu.id_venta = v.id_venta
     JOIN BI_REYES_DE_DATOS.BI_Tiempo t ON t.id_tiempo = t.id_tiempo
-    JOIN BI_REYES_DE_DATOS.BI_Sucursal s ON v.venta_id_sucursal = s.id_sucursal
-    JOIN BI_REYES_DE_DATOS.BI_Ubicacion u ON vu.id_ubicacion = u.id_ubicacion
+    JOIN BI_REYES_DE_DATOS.BI_Sucursal s ON v.id_sucursal= s.id_sucursal
+    JOIN BI_REYES_DE_DATOS.BI_Ubicacion u ON v.id_ubicacion = u.id_ubicacion
 	join REYES_DE_DATOS.Localidad l on u.id_localidad = l.id_localidad
 GROUP BY
-    v.ticket_fecha_hora,
-	l.localidad_nombre,
+	v.id_turno,
+    l.localidad_nombre,
     t.mes,
     t.anio;
 GO
@@ -539,13 +542,11 @@ GO
 ----- 
 CREATE VIEW BI_REYES_DE_DATOS.BI_Porcentaje_Descuento_Por_Mes AS
 SELECT
-	t.anio as Año,
+	(sum(v.cantidad_descuentos) * 100) / (sum(v.cantidad_ventas)) as Porcentaje,
 	t.mes as Mes,
-	(count(distinct v.ticket_total_descuento_aplicado) * 100) / (count (distinct v.id_venta)) as Porcentaje
+	t.anio as Año
 FROM BI_REYES_DE_DATOS.BI_hechos_venta v
-	join BI_REYES_DE_DATOS.BI_hechos_venta_tiempo vt on v.id_ticket = vt.id_venta
-	join BI_REYES_DE_DATOS.BI_Tiempo t on vt.id_tiempo = t.id_tiempo
-WHERE v.ticket_total_descuento_aplicado is not null
+	join BI_REYES_DE_DATOS.BI_Tiempo t on v.id_tiempo = t.id_tiempo
 GROUP BY
 	t.anio,
 	t.mes;
@@ -555,17 +556,13 @@ GO
 -----
 CREATE VIEW BI_REYES_DE_DATOS.Vista_Categorias_Productos_Con_Mayor_Descuento AS
 SELECT top 3
-	t.anio as Anio,
-    t.cuatrimestre as Cuatrimestre,
     pc.producto_categoria_detalle as Categoria,
-	sum(v.ticket_total_descuento_aplicado) as SumatoriaDescuentosAplicados
+	sum(v.monto_descuentos_aplicados) as SumatoriaDescuentosAplicados,
+	t.anio as Anio,
+    t.cuatrimestre as Cuatrimestre
 FROM BI_REYES_DE_DATOS.BI_hechos_venta v
-	join BI_REYES_DE_DATOS.BI_hechos_venta_tiempo vt on v.id_ticket = vt.id_venta
-	join BI_REYES_DE_DATOS.BI_Tiempo t on vt.id_tiempo = t.id_tiempo
-	join BI_REYES_DE_DATOS.BI_hechos_ticket tck on v.id_ticket = tck.id_ticket
-	join REYES_DE_DATOS.Producto p on tck.id_producto = p.id_producto
-	join BI_REYES_DE_DATOS.BI_Producto_categoria pc on p.id_producto_categoria = pc.id_producto_categoria
-WHERE v.ticket_total_descuento_aplicado IS NOT NULL
+	join BI_REYES_DE_DATOS.BI_Tiempo t on v.id_tiempo = t.id_tiempo
+	join BI_REYES_DE_DATOS.BI_Producto_categoria pc on v.id_categoria_prod = pc.id_producto_categoria
 GROUP BY
     t.anio,
 	t.cuatrimestre,
@@ -577,16 +574,13 @@ GO
 CREATE VIEW BI_REYES_DE_DATOS.BI_Porcentaje_Cumplimiento_Envios AS
 SELECT
     s.id_sucursal AS Sucursal,
+	(COUNT(CASE WHEN e.envio_fecha_entrega <= e.envio_fecha_programada THEN 1 ELSE NULL END) * 100.0) / COUNT(e.id_envio) AS PorcentajeDeCumplimiento,
     t.anio AS Año,
-    t.mes AS Mes,
-	(COUNT(CASE WHEN e.envio_fecha_entrega <= e.envio_fecha_programada THEN 1 ELSE NULL END) * 100.0) / COUNT(e.id_envio) AS PorcentajeDeCumplimiento
+    t.mes AS Mes
 FROM BI_REYES_DE_DATOS.BI_hechos_envio e
-    JOIN BI_REYES_DE_DATOS.BI_hechos_venta v ON e.id_ticket = v.id_ticket 
-    JOIN BI_REYES_DE_DATOS.BI_hechos_venta_tiempo vt ON vt.id_venta = v.id_venta
-    JOIN BI_REYES_DE_DATOS.BI_Tiempo t ON t.id_tiempo = t.id_tiempo
-    JOIN BI_REYES_DE_DATOS.BI_Sucursal s ON v.venta_id_sucursal = s.id_sucursal
-where e.envio_fecha_entrega is not null
-    and e.envio_fecha_programada is not null
+    JOIN BI_REYES_DE_DATOS.BI_Tiempo t ON e.id_tiempo = t.id_tiempo
+    JOIN BI_REYES_DE_DATOS.BI_Sucursal s ON e.id_sucursal = s.id_sucursal
+where e.envio_fecha_entrega is not null and e.envio_fecha_programada is not null
 GROUP BY
     s.id_sucursal,
     t.anio,
@@ -597,18 +591,15 @@ GO
 ----- 
 CREATE VIEW BI_REYES_DE_DATOS.BI_Cantidad_Envios_Rango_Etario AS
 SELECT
-    BI_REYES_DE_DATOS.rangoEtario(c.cliente_fecha_nacimiento) as RangoEtarioCliente,
+    count(*) as CantidadEnvios,
+	e.id_rango_etario as RangoEtarioCliente,
     t.cuatrimestre as Cuatrimeste,
-    t.anio as Año,
-    count(*) as CantidadEnvios
+    t.anio as Año
 FROM BI_REYES_DE_DATOS.BI_hechos_envio e
-    JOIN BI_REYES_DE_DATOS.BI_hechos_venta v ON e.id_ticket = v.id_ticket
-    JOIN BI_REYES_DE_DATOS.BI_hechos_venta_tiempo vt ON vt.id_venta = v.id_venta
-    JOIN BI_REYES_DE_DATOS.BI_Tiempo t ON t.id_tiempo = t.id_tiempo 
-	JOIN BI_REYES_DE_DATOS.BI_Cliente c ON e.id_cliente = c.id_cliente
+    JOIN BI_REYES_DE_DATOS.BI_Tiempo t ON e.id_tiempo = t.id_tiempo 
 GROUP BY
-    BI_REYES_DE_DATOS.rangoEtario(c.cliente_fecha_nacimiento),
-    t.cuatrimestre,
+    e.id_rango_etario,
+	t.cuatrimestre,
     t.anio;
 GO
 ----- 
@@ -616,16 +607,14 @@ GO
 ----- 
 CREATE VIEW BI_REYES_DE_DATOS.BI_Top_5_Localidades_Costo_Envio AS
 SELECT TOP 5
-    c.id_cliente as Cliente,
     l.localidad_nombre as Localidad,
     sum(e.envio_costo) as CostoEnvio
 FROM BI_REYES_DE_DATOS.BI_hechos_envio e
-	JOIN BI_REYES_DE_DATOS.BI_Cliente c on e.id_cliente = c.id_cliente
-	JOIN BI_REYES_DE_DATOS.BI_Ubicacion u on c.cliente_id_domicilio = u.id_ubicacion
+	JOIN BI_REYES_DE_DATOS.BI_Ubicacion u on e.id_ubicacion = u.id_ubicacion
 	JOIN REYES_DE_DATOS.Localidad l on u.id_localidad = l.id_localidad
+	INNER JOIN REYES_DE_DATOS.Cliente c on u.id_ubicacion = c.cliente_id_domicilio
 GROUP BY
-    c.id_cliente,
-	l.localidad_nombre;
+    l.localidad_nombre;
 GO
 ----- 
 -- 10) Vista para calcular las 3 sucursales con el mayor importe de pagos en cuotas, según el medio de pago, mes y año.
@@ -635,12 +624,16 @@ CREATE VIEW BI_REYES_DE_DATOS.BI_Top3_Sucursales_Pagos_Cuotas AS
 -- Por eso no pusimos top 3 sino top (11*3*3) = top 99
 SELECT top 99
     s.id_sucursal as Sucursal,
+    sum(p.pago_importe) as MayorImporteCuotas,
     mp.medio_de_pago_clasificacion as MedioDePago,
 	t.mes as Mes,
-    t.anio as Anio,
-    sum(p.pago_importe) as MayorImporteCuotas
+    t.anio as Anio
+FROM BI_REYES_DE_DATOS.BI_hechos_pago p
+	JOIN BI_REYES_DE_DATOS.BI_Tiempo t ON p.id_tiempo = t.id_tiempo	
+	JOIN 
+	--kk
+
 FROM BI_REYES_DE_DATOS.BI_hechos_venta v
-    JOIN BI_REYES_DE_DATOS.BI_hechos_venta_tiempo vt ON vt.id_venta = v.id_venta
     JOIN BI_REYES_DE_DATOS.BI_Tiempo t ON t.id_tiempo = t.id_tiempo
 	join BI_REYES_DE_DATOS.BI_Sucursal s on v.venta_id_sucursal = s.id_sucursal
 	join BI_REYES_DE_DATOS.BI_hechos_ticket tk on v.id_ticket = tk.id_ticket
@@ -893,4 +886,3 @@ FROM REYES_DE_DATOS.Item_Ticket t
 PRINT 'Migración de BI_hechos_ticket terminada'
 GO --kk esto no se que error tiene
 */
-
